c1c8e09ff5564bb7875e7c720f96153879c41806
Joshua Sung <yuniksung@gmail.com>
2020-12-01 21:15:49 -0600

    few style of card has been modified

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -3,18 +3,18 @@ const Employee = require("../lib/Employee");
 
 test("Can set office number from constructor argument", () => {
   const testValue = 100;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  const e = new Manager("Foo", 1, "a@gg.com", testValue);
   expect(e.officeNumber).toBe(testValue);
 });
 
 test('getRole() should return "Manager"', () => {
   const testValue = "Manager";
-  const e = new Manager("Foo", 1, "test@test.com", 100);
+  const e = new Manager("Foo", 1, "a@gg.com", 100);
   expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get office number from getOffice()", () => {
   const testValue = 100;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  const e = new Manager("Foo", 1, "a@gg.com", testValue);
   expect(e.getOfficeNumber()).toBe(testValue);
 });
